ac59be7bfd3d53f9e32de2a0eb6d0565235e40e6
Vladislav-Chistyakov <vladischis@gmail.com>
2025-04-05 16:03:57 +0300

    market: создание страницы магазина

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,14 @@ const fetchServer = async function () {
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-4 text-red">
     <h1 class="text-2xl font-bold mb-4">🗨️ WebSocket Чат</h1>
 
     <div class="border p-3 rounded mb-3 h-60 overflow-y-auto bg-gray-100">
     </div>
 
     <div class="flex gap-2">
-      <button @click="fetchServer">Server Api</button>
+      <button @click="fetchServer" class="bg-purple">Server Api</button>
     </div>
   </div>
 </template>
