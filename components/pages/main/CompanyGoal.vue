62ccf83b766cb84dcd519a2b5d6b0265c20063d1
Vladislav-Chistyakov <vladischis@gmail.com>
2025-04-10 10:29:13 +0300

    market: сверстан блок с целью компании

---
+++ components/pages/main/CompanyGoal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container xl:max-w-[1440px] xl:mx-auto grid grid-cols-[614px_1fr]">
+    <div class="relative p-[183px_74px_174px] bg-[url(@/assets/images/pages/main/background-one.jpeg)] rounded-l-[8px]">
+      <div class="bg-[#000000B2] absolute top-0 bottom-0 left-0 right-0 z-5 rounded-l-[8px]" />
+
+      <div class="relative z-10">
+        <h2 class="mb-[15px] font-core-sans-c font-extrabold text-[34px] leading-[50px] text-white">
+          WE MADE YOUR EVERYDAY FASHION BETTER!
+        </h2>
+
+        <p class="font-causten font-light text-[20px] leading-[24px] mb-[48px] text-white tracking-[1px]">
+          In our journey to improve everyday fashion, euphoria presents EVERYDAY wear range - Comfortable & Affordable fashion 24/7
+        </p>
+
+        <button class="py-[12px] px-[42px] bg-white rounded-[8px] text-text-black text-[18px] leading-[22px] font-semibold">
+          Shop Now
+        </button>
+      </div>
+    </div>
+
+    <div class="bg-[url(@/assets/images/pages/main/peoples.jpg)] rounded-r-[8px]" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,10 @@
       <div class="container xl:max-w-[1440px] xl:mx-auto mb-[130px]">
         Hero
       </div>
+
       <PagesMainPromotions class="mb-[72px]" />
+
+      <PagesMainCompanyGoal class="mb-[80px]" />
       <NuxtPage />
     </main>
     <Footer />
