272a755b3f852703da8f2e36be31f45d36b27d65
Vladislav-Chistyakov <vladischis@gmail.com>
2025-08-27 07:35:12 +0300

    market: Разделение меню фильтров

---
+++ components/pages/products/MenuAside.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="border-x border-gray-border max-h-fit">
+    <div
+      class="flex items-center justify-between gap-[14px] p-[20px_30px_30px_30px] border-b border-gray-border font-semibold"
+    >
+      <h2 class="text-[22px] leading-[26px] text-text-gray-200">Filter</h2>
+
+      <img src="@/assets/images/pages/product/filter.svg" alt="filter icon" />
+    </div>
+
+    <client-only>
+      <slot name="filter" />
+    </client-only>
+
+    <div class="border-b border-gray-border pb-[30px]">списко фильтров</div>
+  </aside>
+</template>
+
+<style scoped></style>
--- pages/products/[gender]/index.vue
+++ pages/products/[gender]/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useProductsStore } from '~/store/products'
 import ListClothes from '~/components/pages/products/ListClothes.vue'
+import ProductsMenuAside from '~/components/pages/products/MenuAside.vue'
 
 const productsStore = useProductsStore()
 const route = useRoute()
@@ -19,24 +20,11 @@ const productsList = computed(() => {
 <template>
   <div class="container xl:max-w-[1440px] font-causten">
     <div class="grid grid-cols-[295px_1fr] gap-[50px]">
-      <aside class="border-x border-gray-border max-h-fit">
-        <div
-          class="flex items-center justify-between gap-[14px] p-[20px_30px_30px_30px] border-b border-gray-border font-semibold"
-        >
-          <h2 class="text-[22px] leading-[26px] text-text-gray-200">Filter</h2>
-
-          <img
-            src="@/assets/images/pages/product/filter.svg"
-            alt="filter icon"
-          />
-        </div>
-
-        <client-only>
-          <FilterCategoriesMenu :products-list="productsList" />
-        </client-only>
-
-        <div class="border-b border-gray-border pb-[30px]">списко фильтров</div>
-      </aside>
+      <ProductsMenuAside>
+        <template #filter>
+          <FilterCategoriesMenu />
+        </template>
+      </ProductsMenuAside>
 
       <ListClothes :list="productsList" title="Products" class="mt-[33px]" />
     </div>
