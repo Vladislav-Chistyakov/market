de206c273ce3cfe7f8ffc4389c81437b8a6e0e22
Vladislav-Chistyakov <vladischis@gmail.com>
2025-09-11 09:59:20 +0300

    main: Сделана базовая валидация для создание заявки

--- components/pages/placing-an-order/FormCreateOrder.vue
+++ components/pages/placing-an-order/FormCreateOrder.vue
@@ -1,20 +1,90 @@
 <script setup lang="ts">
 import type { Reactive } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, helpers } from '@vuelidate/validators'
+
+const emit = defineEmits(['submit-form'])
 
 const pending = ref(false)
 
 type Form = {
-  name: string
+  name: string | null,
+  lastName: string | null,
+  countryAndRegion: string | null,
+  companyName?: string | null,
+  streetAddress: string | null,
+  aptSuiteUnit?: string | null,
+  city: string | null,
+  state: string | null,
+  postalCode: string | null,
+  phone: string | null,
 }
 
 const form: Reactive<Form> = reactive({
-  name: 'Turtleneck',
+  name: null,
+  lastName: null,
+  countryAndRegion: null,
+  companyName: null,
+  streetAddress: null,
+  aptSuiteUnit: null,
+  city: null,
+  state: null,
+  postalCode: null,
+  phone: null,
 })
 
-const emit = defineEmits(['submit-form'])
+const rules = {
+  name: {
+    required: helpers.withMessage('Fill in the name field', required)
+  },
+  lastName: {
+    required: helpers.withMessage('Fill in the lastname field', required)
+  },
+  countryAndRegion: {
+    required: helpers.withMessage('Fill in the country and region field', required)
+  },
+  streetAddress: {
+    required: helpers.withMessage('Fill in the street address field', required)
+  },
+  city: {
+    required: helpers.withMessage('Fill in the city field', required)
+  },
+  state: {
+    required: helpers.withMessage('Fill in the state field', required)
+  },
+  postalCode: {
+    required: helpers.withMessage('Fill in the postal code field', required)
+  },
+  phone: {
+    required: helpers.withMessage('Fill in the phone field', required)
+  },
+}
+
+const v$ = useVuelidate(rules, form)
 
 const resetForm = function () {
-  form.name = ''
+  form.name = null
+  form.lastName = null
+  form.countryAndRegion = null
+  form.companyName = null
+  form.streetAddress = null
+  form.aptSuiteUnit = null
+  form.city = null
+  form.state = null
+  form.postalCode = null
+  form.phone = null
+}
+
+async function submitForm () {
+  pending.value = true
+  if (await v$.value.$validate()) {
+    console.log('submitForm', form)
+    emit('submit-form', form)
+  } else {
+    return
+  }
+  pending.value = false
+
 }
 </script>
 
@@ -24,7 +94,7 @@ const resetForm = function () {
       Billing Details
     </b>
 
-    <form class="grid grid-cols-2 gap-x-[38px] gap-y-[29px]">
+    <form @submit.prevent="submitForm" class="grid grid-cols-2 gap-x-[38px] gap-y-[29px]">
       <div>
         <UniversalBaseInput
           :disabled="pending"
@@ -39,6 +109,17 @@ const resetForm = function () {
               First Name*
             </p>
           </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.name.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
+          </template>
         </UniversalBaseInput>
       </div>
 
@@ -47,14 +128,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Last Name"
-          :value="form.name"
+          :value="form.lastName"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.lastName = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               Last Name*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.lastName.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -64,14 +156,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Country / Region"
-          :value="form.name"
+          :value="form.countryAndRegion"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.countryAndRegion = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               Country / Region*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.countryAndRegion.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -81,9 +184,9 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Company (optional)"
-          :value="form.name"
+          :value="form.companyName"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.companyName = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
@@ -98,14 +201,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="House number and street name"
-          :value="form.name"
+          :value="form.streetAddress"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.streetAddress = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               Street Address*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.streetAddress.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -115,9 +229,9 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="apartment, suite, unit, etc. (optional)"
-          :value="form.name"
+          :value="form.aptSuiteUnit"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.aptSuiteUnit = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
@@ -132,14 +246,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Town / City"
-          :value="form.name"
+          :value="form.city"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.city = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               City*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.city.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -149,14 +274,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="State"
-          :value="form.name"
+          :value="form.state"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.state = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               State*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.state.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -166,14 +302,25 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Postal Code"
-          :value="form.name"
+          :value="form.postalCode"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.postalCode = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               Postal Code*
             </p>
+          </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.postalCode.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
           </template>
         </UniversalBaseInput>
       </div>
@@ -183,23 +330,33 @@ const resetForm = function () {
           :disabled="pending"
           type="text"
           placeholder="Phone"
-          :value="form.name"
+          :value="form.phone"
           style-input="gray"
-          @update:value="form.name = $event"
+          @update:value="form.phone = $event"
         >
           <template #label>
             <p class="font-causten mb-[9px] text-[16px] leading-[18px] text-black font-semibold">
               Phone*
             </p>
           </template>
+
+          <template #error-message>
+            <div class="absolute bottom-[-14px] right-0 text-red text-[11px] leading-3">
+              <p
+                v-for="error of v$.phone.$errors"
+                :key="error.$uid"
+              >
+                {{ error.$message }}
+              </p>
+            </div>
+          </template>
         </UniversalBaseInput>
       </div>
 
+      <button class="bg-purple block w-fit p-[13px_20px] rounded-[8px] text-white text-[18px] font-causten font-light">
+        Continue to delivery
+      </button>
     </form>
-
-    <button @click="emit('submit-form')" class="bg-purple block w-fit p-[13px_20px] rounded-[8px] text-white text-[18px] font-causten font-light">
-      Continue to delivery
-    </button>
   </div>
 </template>
 
